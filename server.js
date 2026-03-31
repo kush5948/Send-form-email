b680fbd5e2d47eeddfb369680bfec2ff86ea65e8
kushal <jkushal208@gmail.com>
2026-03-31 16:15:47 +0530

    fix the contoller folder import sync locally and github

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { rateLimit } from 'express-rate-limit'
-import EmailSendTrust from './controllers/EmailSendTrust.js';
-import EmailSend from './controllers/EmailSend.js';
-import EmailSendSchool from './controllers/EmailSendSchool.js';
+import EmailSendTrust from './Controllers/EmailSendTrust.js';
+import EmailSend from './Controllers/EmailSend.js';
+import EmailSendSchool from './Controllers/EmailSendSchool.js';
 
 
 dotenv.config();
